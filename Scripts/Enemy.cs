8f17416d4411d720f7907f76804055f0cd392145
Tylertone1 <60562575+Tylertone1@users.noreply.github.com>
2023-01-09 16:56:21 -0800

    Add files via upload

---
+++ Scripts/Enemy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public Rigidbody2D m_toge;
+    public int m_HP;
+    public float speed = -10f;
+    public int MaxTurns = 3;
+    private int turns = 0;
+    public GameObject m_player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public GameObject ui_congrats;
+
+    void BeShot(int damage)
+    {
+        m_HP -= damage;
+        if (m_HP == 0)
+        {
+            Destroy(gameObject);
+            turns++;
+            print("success");
+            Vector2 v = new Vector2( 0,0);
+            m_toge.velocity = v * Time.deltaTime;
+            ui_congrats.SetActive(true);
+
+
+
+           
+
+        }
+    }
+
+   
+}
---
+++ Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject m_player;
+    // Start is called before the first frame update
+
+
+
+    private void Awake()
+    {
+       
+        //Cursor.visible = false;
+    }
+    void Start()
+    {
+        Vector3 vec = Vector3.zero;
+
+        vec.x = PlayerPrefs.GetFloat("CameraPosX", 0.5f);
+        vec.y = PlayerPrefs.GetFloat("CameraPosY",0.5f);
+        vec.z = PlayerPrefs.GetFloat("CameraPosZ", -10);
+        Camera.main.transform.position = vec;
+
+        vec.x = PlayerPrefs.GetFloat("PlayerPosX", -8.5f);
+        vec.y = PlayerPrefs.GetFloat("PlayerPosY", 7.5f);
+        vec.z = PlayerPrefs.GetFloat("PlayerPosZ", 0);
+        m_player.transform.position = vec;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            PlayerPrefs.DeleteAll();
+        }
+    }
+}
